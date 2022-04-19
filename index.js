dd3c5a25510fd143026e4a61fb14854f73cc17c8
unknown <byoo@fasoo.com>
2022-04-19 17:27:01 +0900

    style: omg background

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
 const path = require("path");
 
 const createWindow = () => {
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function getTime() {
   const nowDate = new Date();
   time = nowDate.toLocaleTimeString();
   const Container = document.querySelector(".Container");
+  Container.classList.add("AlertContainer");
   Container.innerHTML = "";
   const curTime = document.createElement("div");
   curTime.innerText = time;
@@ -28,7 +29,16 @@ function getTime() {
   const leftTime = document.createElement("div");
   leftTime.innerText = leftTimeStr;
   leftTime.classList.add("Card");
-
+  const testDiffMin = diffMin - 10;
+  if (parseInt(diffHour) < 1) {
+    if (parseInt(testDiffMin) < 30) {
+      if (App.classList.contains("AlertAppBackGround"))
+        App.classList.remove("AlertAppBackGround");
+    } else {
+      App.classList.toggle("DefaultAppBackGround");
+      App.classList.toggle("AlertAppBackGround");
+    }
+  }
   const Title = document.createElement("div");
   Title.innerText = "남은 시간 알려줘";
   Container.appendChild(Title);
@@ -36,3 +46,35 @@ function getTime() {
   Container.appendChild(curTime);
 }
 setInterval(getTime, 1000);
+
+const App = document.querySelector(".App");
+
+App.classList.add("DefaultAppBackGround");
+
+function setDangerBackGround() {
+  const nowDate = new Date();
+  const finDate = new Date(
+    nowDate.getFullYear(),
+    nowDate.getMonth(),
+    nowDate.getDate(),
+    18,
+    00,
+    00
+  );
+  const diff = finDate - nowDate;
+  const diffHour = String(Math.floor((diff / (1000 * 60 * 60)) % 24)).padStart(
+    2,
+    "0"
+  );
+  const diffMin = String(Math.floor((diff / (1000 * 60)) % 60)).padStart(
+    2,
+    "0"
+  );
+  const testDiff = diffMin - 10;
+  if (parseInt(testDiff) < 30 && parseInt(diffHour) < 1) {
+    App.classList.toggle("DefaultAppBackGround");
+    App.classList.toggle("DangerAppBackGround");
+  }
+}
+
+setInterval(setDangerBackGround, 500);
